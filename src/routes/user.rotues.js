fdf970fe5ea09dd620ec2f62c68bc7693b7e4ce6
Sunil Sharma <84322038+SunilKumarSharma129@users.noreply.github.com>
2025-08-22 18:45:29 +0530

    Update user.rotues.js

--- src/routes/user.rotues.js
+++ src/routes/user.rotues.js
@@ -10,19 +10,19 @@ const router = Router();
 
 
 //unsecured route
-router.route("/register").post(
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-    {
-      name: "coverImage",
-      maxCount: 1,
-    },
-  ]),
-  registerUser
-);
+// router.route("/register").post(
+//   upload.fields([
+//     {
+//       name: "avatar",
+//       maxCount: 1,
+//     },
+//     {
+//       name: "coverImage",
+//       maxCount: 1,
+//     },
+//   ]),
+//   registerUser
+// );
 
 router.route("/login").post(logoutUser)
 router.route("/refresh-token").post(refreshAccessToken);
@@ -32,12 +32,7 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
-router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
-
-router.route("/update-account").patch(verifyJWT, updateAccountDetails);
-
-router.route("/avatar").patch(verifyJWT,upload.single("avatar") ,updateUserAvatar);
-
+ 
 router
   .route("/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
